c304af7b262204bb6230b8ef1eba82841b6fb3e2
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-11-05 02:19:44 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import css from './Modal.module.css';
 
 export const Modal = ({ image, onCloseModal }) => {
-  const onKeyDown = (event) => {
+  const onKeyDown = useCallback((event) => {
     if (event.code === 'Escape') {
       onCloseModal();
     }
-  };
+  }, [onCloseModal]);
 
   const onOverlayClick = (event) => {
     if (event.target === event.currentTarget) {
@@ -22,7 +22,7 @@ export const Modal = ({ image, onCloseModal }) => {
       document.removeEventListener('keydown', onKeyDown);
       document.body.style.overflow = 'auto';
     };
-  }, []); 
+  }, [onKeyDown]);
 
   return (
     <div className={css.overlay} onClick={onOverlayClick}>
@@ -32,3 +32,39 @@ export const Modal = ({ image, onCloseModal }) => {
     </div>
   );
 };
+
+
+// import React, { useEffect } from 'react';
+// import css from './Modal.module.css';
+
+// export const Modal = ({ image, onCloseModal }) => {
+//   const onKeyDown = (event) => {
+//     if (event.code === 'Escape') {
+//       onCloseModal();
+//     }
+//   };
+
+//   const onOverlayClick = (event) => {
+//     if (event.target === event.currentTarget) {
+//       onCloseModal();
+//     }
+//   };
+
+//   useEffect(() => {
+//     document.addEventListener('keydown', onKeyDown);
+//     document.body.style.overflow = 'hidden';
+
+//     return () => {
+//       document.removeEventListener('keydown', onKeyDown);
+//       document.body.style.overflow = 'auto';
+//     };
+//   }, []); 
+
+//   return (
+//     <div className={css.overlay} onClick={onOverlayClick}>
+//       <div className={css.modal}>
+//         <img className={css.largeImage} src={image.largeImageURL} alt={image.tags} />
+//       </div>
+//     </div>
+//   );
+// };
